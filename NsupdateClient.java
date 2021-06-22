9e8c808645c251cf012730982a323ff6ae5340c0
Fabian <a@b.c>
2021-06-23 00:20:23 +0200

    Dualstack-Support

--- NsupdateClient.java
+++ NsupdateClient.java
@@ -6,63 +6,83 @@ import java.net.http.HttpResponse;
 
 public class NsupdateClient {
 
-    private String hostAddress = "yourHostname.nsupdate.info";
-    private String password = "yourSecret";
-    private String lastUpdatedIPv4 = null;
+    private String hostAddress = "domain";
+    private String password = "pw";
+    private InetAddress lastUpdatedIPv4 = null;
+    private InetAddress lastUpdatedIPv6 = null;
 
     public NsupdateClient() throws InterruptedException {
         while(true){
-            try{
-                checkIPv4();
-            }catch(Exception exc){
-                System.err.println("Error: " + exc.getMessage());
-            }
+            checkIP();
             Thread.sleep(30000);
         }
     }
-
-    private void checkIPv4(){ //Call this method in intervals
-        if(lastUpdatedIPv4 == null){ //on first start or error, get IP from DNS
-            lastUpdatedIPv4 = getIPv4Address(hostAddress);
+    private boolean testNewAddress(InetAddress oldIP,InetAddress newIP){
+        System.out.println("OldIP: "+oldIP+" NewIP: "+newIP);
+        return oldIP == null || (newIP != null && oldIP.hashCode() != newIP.hashCode());
+    }
+    private void updatefromDNS(){
+        InetAddress iplist[];
+        try{
+            iplist=Inet6Address.getAllByName(hostAddress);
         }
-        String currentIP = getCurrentIPv4();
-        //is update needed?
-        if(lastUpdatedIPv4 != null && currentIP != null && ! lastUpdatedIPv4.equals(currentIP)){
-            //update (only when currentIP != null and lastUpdatedIPv4 != null)
-            lastUpdatedIPv4 = updateIPv4(this.hostAddress, this.password);
+        catch(UnknownHostException e){
+            iplist=new InetAddress[0];
         }
+        for(InetAddress t : iplist)
+            if(t instanceof Inet6Address)
+                lastUpdatedIPv6 = t;
+            else if(t instanceof Inet4Address)
+                lastUpdatedIPv4 = t;
     }
 
-    private String getIPv4Address(String of){
+    private void checkIP(){ //Call this method in intervals
+        if(lastUpdatedIPv4 == null || lastUpdatedIPv6 == null){ //on first start or error, get IP from DNS
+            updatefromDNS();
+        }
+        Inet4Address currentIP4 = null;
+        Inet6Address currentIP6 = null;
         try {
-            InetAddress address = InetAddress.getByName(of);
-            String ip = address.toString();
-            return ip.substring(ip.indexOf("/")+1,ip.length());
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-            return null;
+            InetAddress a = getCurrentIP('4');
+            currentIP4 = (Inet4Address) a;
+            a = getCurrentIP('6');
+            currentIP6 = (Inet6Address) a;
+        } catch (ClassCastException e)
+        {
+            System.err.println("Wrong IP version! This should not happen.");
+            System.exit(1);
+        }
+        //is update needed?
+        if( testNewAddress(lastUpdatedIPv4, currentIP4) ){
+            //update (only when currentIP != null and lastUpdatedIPv4 != null)
+            lastUpdatedIPv4 = updateIP(this.hostAddress, this.password,currentIP4,'4');
+        }
+        if(testNewAddress(lastUpdatedIPv6, currentIP6)){
+            lastUpdatedIPv6 = updateIP(this.hostAddress, this.password,currentIP6,'6');
         }
     }
 
-    private String getCurrentIPv4() {
+    private InetAddress getCurrentIP(char version) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://ipv4.nsupdate.info/myip"))
+                .uri(URI.create("https://ipv"+version+".nsupdate.info/myip"))
                 .build();
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             //System.out.println(response.statusCode());
             //System.out.println(response.body());
-            return response.body();
+            return InetAddress.getByName(response.body());
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        } /*catch (UnknownHostException e) {
+           System.err.println("\""+"response.body()"+"\""+"Is no IPv"+version+" address");
+        }*/
         return null;
     }
 
-    private String updateIPv4(String of, String password) {
+    private InetAddress updateIP(String of, String password,InetAddress addr,char version) {
         HttpClient client = HttpClient.newBuilder()
                 .authenticator(new Authenticator() {
                     public PasswordAuthentication getPasswordAuthentication() {
@@ -71,7 +91,7 @@ public class NsupdateClient {
                 })
                 .build();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://ipv4.nsupdate.info/nic/update"))
+                .uri(URI.create("https://ipv"+version+".nsupdate.info/nic/update"))
                 .build();
 
         try {
@@ -81,10 +101,10 @@ public class NsupdateClient {
             String responseString = response.body();
             if(response.statusCode() == 200 && responseString.startsWith("good ")) {
                 System.out.println("Updated with: " + responseString);
-                return responseString.substring(5);
+                return Inet6Address.getByName(responseString.substring(5));
             }else if(response.statusCode() == 200 && responseString.startsWith("nochg ")){
                 System.out.println("Updated with: " + responseString);
-                return responseString.substring(6);
+                return Inet6Address.getByName(responseString.substring(6));
             }else{
                 System.err.println("Error: " + responseString);
                 return null;
